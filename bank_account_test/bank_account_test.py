f8260c69669ccab8d9ceed66b457159adc682cc7
Nathan Silva Ferreira <116688602+nathansilva009@users.noreply.github.com>
2024-10-26 19:42:37 +0000

    basic project, no implementation

---
+++ bank_account_test/bank_account_test.py
@@ -0,0 +1,11 @@
+from bank_account import BankAccount
+
+def test_a_unit_test():
+    # Arrange (Given)
+    print("Arrange something...")
+
+    # Act (When)
+    print("Execute something...")
+
+    # Assert (Then)
+    print("Assert something...")

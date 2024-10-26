d9dd9f2ee4e62ee8244bd42bddf620802f2b6bb1
Nathan Silva Ferreira <116688602+nathansilva009@users.noreply.github.com>
2024-10-26 19:53:03 +0000

    basic functionalities implemented, with unit tests

--- bank_account/bank_account.py
+++ bank_account/bank_account.py
@@ -1,3 +1,17 @@
 class BankAccount:
-    print("Implement something...")
-    
+    def __init__(self, initial_balance):
+        self.balance = initial_balance
+
+    def get_balance(self):
+        return self.balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            raise InsufficientBalanceException()
+        self.balance -= amount
+
+class InsufficientBalanceException(Exception):
+    pass
--- bank_account_test/bank_account_test.py
+++ bank_account_test/bank_account_test.py
@@ -1,11 +1,33 @@
-from bank_account import BankAccount
+import pytest
+from bank_account import BankAccount, InsufficientBalanceException
 
-def test_a_unit_test():
+def test_deposit_increases_balance():
     # Arrange (Given)
-    print("Arrange something...")
+    bank_account = BankAccount(1000)
 
     # Act (When)
-    print("Execute something...")
+    bank_account.deposit(200)
 
     # Assert (Then)
-    print("Assert something...")
+    assert bank_account.get_balance() == 1200
+
+def test_withdraw_decreases_balance():
+    # Arrange (Given)
+    bank_account = BankAccount(1000)
+
+    # Act (When)
+    bank_account.withdraw(200)
+
+    # Assert (Then)
+    assert bank_account.get_balance() == 800
+
+def test_overdrawing_not_allowed():
+    # Arrange (Given)
+    bank_account = BankAccount(1000)
+
+    # Act (When) & Assert (Then)
+    with pytest.raises(InsufficientBalanceException):
+        bank_account.withdraw(1100)
+
+    # Assert (Then)
+    assert bank_account.get_balance() == 1000
